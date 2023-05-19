fb1931d0e814d151f2b6e8f3b7b11100678b5e61
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-20 00:35:56 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 //middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmqu4oo.mongodb.net/?retryWrites=true&w=majority`;
 
