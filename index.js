8a21b059ade5c2753e464f9b55737fc533b8e2ae
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-20 01:06:30 +0600

    posting functionality for seller toys to mongodb successful

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
+
+        const toyCollection = client.db('toyWizards').collection('toys');
+
+        app.post('/seller-toys', async (req, res) => {
+            const sellerToys = req.body;
+            const result = await toyCollection.insertOne(sellerToys);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
