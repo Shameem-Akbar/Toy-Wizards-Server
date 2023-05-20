bc267e9b8b1f86f3ac886e5ed6a05b9581c48384
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-20 23:04:42 +0600

    limit added for all toys page

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
 
 
         app.get('/toys', async (req, res) => {
-            const result = await toyCollection.find().toArray();
+            const result = await toyCollection.find().limit(20).toArray();
             res.send(result);
         })
 
